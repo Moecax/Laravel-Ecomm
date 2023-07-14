729142df5ba6fd3c13a854c2fd571d9a8be02718
Moecax <moseskwereban@gmail.com>
2023-07-14 20:42:32 +0300

    Product model create

--- onlineStore/app/Models/Product.php
+++ onlineStore/app/Models/Product.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    /**
+* PRODUCT ATTRIBUTES
+* $this->attributes['id'] - int - contains the product primary key (id)
+* $this->attributes['name'] - string - contains the product name
+* $this->attributes['description'] - string - contains the product description
+* $this->attributes['image'] - string - contains the product image
+* $this->attributes['price'] - int - contains the product price
+* $this->attributes['created_at'] - timestamp - contains the product creation date
+*$this->attributes['updated_at'] - timestamp - contains the product update time
+*/
 }
